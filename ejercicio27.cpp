4bd626ad60c49f1a5e0b1908d620dfdfdf9b2eeb
nbahamonn <n.bahamonn@uniandes.edu.co>
2019-11-12 18:48:18 +0000

    Clase27

--- ejercicio27.cpp
+++ ejercicio27.cpp
@@ -1,3 +1,52 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+void Explicito(int i, float f, float dx, float w, string archivo);
+void Implicito(int i, float f, float dx, float w, string archivo);
+    
+int main(){
+    
+    float w = 0.1;
+    float f = 4/w;
+    int i = 0;
+    float dx1 = 0.1/w;
+    float dx2 = 0.01/w;
+    float dx3 = 1/w;
+        
+    Explicito(i, f, dx1, w, "Explicito1.dat");
+    Explicito(i, f, dx2, w, "Explicito2.dat");
+    Explicito(i, f, dx3, w, "Explicito3.dat");
+    Implicito(i, f, dx1, w, "Implicito1.dat");
+    Implicito(i, f, dx2, w, "Implicito2.dat");
+    Implicito(i, f, dx3, w, "Implicito3.dat");
+    return 0;
+}
+
+void Explicito(int i, float f, float dx, float w, string archivo){
+    ofstream outfile;
+    outfile.open(archivo);
+    
+    float y = 1;
+ 
+    for (i; i < f; i+=dx){
+        y = y - dx * w * y;
+        outfile << i << " " << y << endl;
+    }
+  outfile.close(); 
+}
+
+void Implicito(int i, float f, float dx, float w, string archivo){
+    
+    ofstream outfile;
+    outfile.open(archivo);
+    
+    float y = 1;
+  
+    for (i; i < f; i+=dx){
+        y = y / (1 + dx * w);
+        outfile << i << " " << y << endl;
+  }
+  outfile.close(); 
+}
